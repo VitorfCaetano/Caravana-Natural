a520b650fe1a87f7a1212d216d950f8970cd9bc3
Vitor Felipe <viitorcaetano@gmail.com>
2018-09-09 23:50:58 -0300

    Fix Header import

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header/Header'
+import Header from '../components/header/header'
 import Footer from '../components/footer/Footer'
 
 import '../scss/index.scss'
